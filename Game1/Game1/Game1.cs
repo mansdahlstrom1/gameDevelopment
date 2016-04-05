3d9322cc53b3c8eaf414a701ed2ffe39ff6f54ba
Ellen Nilsson <ellennilsson@live.se>
2016-04-05 11:42:16 +0200

    FPS kinda fixedd

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -116,23 +116,21 @@ namespace Game1
         {
 
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            
-
-
+           
             spriteBatch.Begin();
 
-            //FPS counter                     
-            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-            frameCounter.Update(deltaTime);
-            var fps = string.Format("FPS: {0}", frameCounter.AverageFramesPerSecond);
-            spriteBatch.DrawString(font, fps, new Vector2(10, 50), Color.White);
+     
 
             myBackground.Draw(spriteBatch);
             spriteBatch.Draw(shuttle, new Vector2(heroShipX, heroShipY));
             spriteBatch.DrawString(font, "Score: " + score, new Vector2(10, 10), Color.White);
             spriteBatch.DrawString(font, "Speed: " + speed, new Vector2(10, 30), Color.White);
-           // spriteBatch.DrawString(font, "FPS: " + (1000 / gameTime.ElapsedGameTime.Milliseconds), new Vector2(10, 50), Color.White);
+            // spriteBatch.DrawString(font, "FPS: " + (1000 / gameTime.ElapsedGameTime.Milliseconds), new Vector2(10, 50), Color.White);
+            //FPS counter                     
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            frameCounter.Update(deltaTime);
+            var fps = string.Format("FPS: {0}", frameCounter.AverageFramesPerSecond);
+            spriteBatch.DrawString(font, fps, new Vector2(10, 50), Color.White);
             spriteBatch.DrawString(font, "heroShip position X,Y: " + heroShipX + "," + heroShipY, new Vector2(10, 70), Color.White);
 
             spriteBatch.End();
