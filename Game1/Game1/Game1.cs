85ea7fe6745841c3dcccc9d03307709c4da51660
Poliky4 <poliky44@hotmail.com>
2016-04-07 14:09:21 +0200

    input, ship & missile

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -28,6 +28,7 @@ namespace Game1
         private Background gameBackground;
         private Background menuBackground;
         private Texture2D shuttle;
+        private Texture2D missileTexture;
         private SpriteFont font;
         private Texture2D startButton;
         private Texture2D exitButton;
@@ -35,12 +36,11 @@ namespace Game1
         private Texture2D resumeButton;
         private Texture2D loadingScreen;
         private int score = 0;
-        private float heroShipY = 0, heroShipX = 0;
         private float speed = 3;
         private List<Player> players = new List<Player>();
         private FrameCounter frameCounter = new FrameCounter();
         private Texture2D spriteFont;
-       
+
         private GameState gameState;
 
         bool paused = false;
@@ -70,6 +70,10 @@ namespace Game1
                 p.Shuttle = Content.Load<Texture2D>("images/DogpoolPortrait");
             }
             gameState = GameState.Playing;
+
+            inputHelper = new InputHelper();
+            ship = new Ship(50, 50);
+
             base.Initialize();
         }
 
@@ -92,8 +96,13 @@ namespace Game1
             menuBackground.Load(GraphicsDevice, menuBackgroundImage);
 
             shuttle = Content.Load<Texture2D>("images/DogpoolPortrait");
-
+            
             font = Content.Load<SpriteFont>("myFont"); // Use the name of your sprite font file here instead of 'Score'.
+
+            missileTexture = Content.Load<Texture2D>("images/laser_small");
+
+            ship.MissileTexture = missileTexture;
+            ship.Texture = shuttle;
 
 
             // TODO: use this.Content to load your game content here
@@ -119,25 +128,28 @@ namespace Game1
 
             if (gameState == GameState.Playing)
             {
-                // The time since Update was called last.
-                float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            // The time since Update was called last.
+            float elapsed = (float)gameTime.ElapsedGameTime.TotalSeconds;
 
-                // TODO: Add your game logic here.
+            // TODO: Add your game logic here.
                 gameBackground.Update(elapsed * 200);
 
-                score++;
+            
 
-                //CheckInput();
+            score++;
 
-                if (score % 1000 == 1 && speed <= 12)
-                {
-                    speed += 3;
-                }
+            inputHelper.CheckController(GamePad.GetState(PlayerIndex.One, GamePadDeadZone.Circular), ship);
+            inputHelper.CheckKeyboard(Keyboard.GetState(), ship);
 
-
-                base.Update(gameTime);
-
+            if (score % 1000 == 1 && speed <= 12)
+            {
+                speed += 3;
             }
+ 
+
+            base.Update(gameTime);
+
+        }
 
         }
 
@@ -147,20 +159,30 @@ namespace Game1
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+           
             spriteBatch.Begin();
             if (gameState == GameState.Playing)
             {
 
                 gameBackground.Draw(spriteBatch);
                 spriteBatch.Draw(shuttle, new Vector2(heroShipX, heroShipY));
-                spriteBatch.DrawString(font, "Score: " + score, new Vector2(10, 10), Color.White);
-                spriteBatch.DrawString(font, "Speed: " + speed, new Vector2(10, 30), Color.White);
-                // spriteBatch.DrawString(font, "FPS: " + (1000 / gameTime.ElapsedGameTime.Milliseconds), new Vector2(10, 50), Color.White);
-                //FPS counter                     
-                var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
-                frameCounter.Update(deltaTime);
-                var fps = string.Format("FPS: {0}", frameCounter.AverageFramesPerSecond);
-                spriteBatch.DrawString(font, fps, new Vector2(10, 50), Color.White);
+            spriteBatch.DrawString(font, "Score: " + score, new Vector2(10, 10), Color.White);
+            spriteBatch.DrawString(font, "Speed: " + speed, new Vector2(10, 30), Color.White);
+            // spriteBatch.DrawString(font, "FPS: " + (1000 / gameTime.ElapsedGameTime.Milliseconds), new Vector2(10, 50), Color.White);
+            //FPS counter                     
+            var deltaTime = (float)gameTime.ElapsedGameTime.TotalSeconds;
+            frameCounter.Update(deltaTime);
+            var fps = string.Format("FPS: {0}", frameCounter.AverageFramesPerSecond);
+            spriteBatch.DrawString(font, fps, new Vector2(10, 50), Color.White);
                 spriteBatch.DrawString(font, "heroShip position X,Y: " + heroShipX + "," + heroShipY, new Vector2(10, 70), Color.White);
 
             } else if (gameState == GameState.StartMenu){
@@ -179,6 +201,9 @@ namespace Game1
                 // TODO
                 // Loading
 
+            foreach(Missile m in ship.Missiles){
+                spriteBatch.Draw(m.Texture, new Vector2(m.XPos, m.YPos));
+                m.Move();
             }
 
             spriteBatch.End();
@@ -296,7 +321,7 @@ namespace Game1
                 {
                     Exit();
                 }
-
+            
                 //Left Stick
                 if (GamePad.GetState(PlayerIndex.One).Buttons.LeftStick == ButtonState.Pressed)
                 {
@@ -320,7 +345,7 @@ namespace Game1
                 if (GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y < 0 && ShipIsWithinLimits(heroShipX, heroShipY))
                 {
                     heroShipY -= (GamePad.GetState(PlayerIndex.One).ThumbSticks.Left.Y * speed);
-                }
+        }
 
                 //Right Stick
                 if (GamePad.GetState(PlayerIndex.One).Buttons.RightStick == ButtonState.Pressed)
@@ -356,7 +381,7 @@ namespace Game1
             if (playerX >= 0 && playerX < (800 - 40) && playerY >= 0 && playerY < (480 - 40))
             {
                 return true;
-            }
+            } 
 
 
             return false;
