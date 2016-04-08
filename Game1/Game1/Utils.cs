06e08571ee518bfd0e0a88a94d799b99d1ac4300
Måns Dahlström <mans@mansdahlstrom.se>
2016-04-08 23:55:28 +0200

    Updated Button and Utils Class and added buttons for bothy paused and Menu gamestate

--- Game1/Game1/Utils.cs
+++ Game1/Game1/Utils.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,15 +12,50 @@ namespace Game1
     class Utils
     {
 
-        public int[] centerText(int textHeight, int textWidth)
+        private int _fontMarginLeft;
+        public int fontMarginLeft
         {
-            int[] postition = new int[2];
+            get { return _fontMarginLeft; }
+            set { _fontMarginLeft = value; }
+        }
+        private int _fontMarginTop;
+        public int fontMarginTop
+        {
+            get { return _fontMarginTop; }
+            set { _fontMarginTop = value; }
+        }
+
+
+        public int[] centerText(SpriteFont font, String text)
+        {
+            Vector2 fontDimensions = font.MeasureString(text);
+
+            int[] margins = new int[2];
             // TOP
-            postition[0] = (textHeight - 40) / 2;
+            // Static width for button is 100
+            // TODO
+            // Get a responsive width from button object.
+            this.fontMarginLeft = (int)(100 - fontDimensions.X) / 2;
 
-            postition[1] = (textWidth - 100) / 2;
+            //LEFT
+            // Static width for button is 23
+            // TODO
+            // Get a responsive width from button object.
+            this.fontMarginTop = (int)(23 - fontDimensions.Y) / 2;
 
-            return postition;
+            if (this.fontMarginLeft < 0)
+            {
+                this.fontMarginLeft = 0;
+            }
+            if (this.fontMarginTop < 0)
+            {
+                this.fontMarginTop = 0;
+            }
+
+
+            margins[0] = this.fontMarginLeft;
+            margins[1] = this.fontMarginTop;
+            return margins;
         }
     }
 }
