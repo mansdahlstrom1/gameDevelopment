6126d582ebfb9fb6fa5b180900db49155f583716
Poliky4 <poliky44@hotmail.com>
2016-04-09 22:50:54 +0200

    test

--- Game1/Game1/Ship.cs
+++ Game1/Game1/Ship.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework.Graphics;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
 using System.Collections.Generic;
@@ -8,8 +9,8 @@ using System.Threading.Tasks;
 
 namespace Game1
 {
-    //abstract
-    class Ship
+
+    abstract class Ship
     {
         //private Point position;
         private float xPos;
@@ -20,28 +21,18 @@ namespace Game1
         private Texture2D texture;
         public Texture2D Texture { get { return texture; } set { texture = value; } }
 
-        //private Sprite sprite; //Replaces Texture2D
-        //public Sprite Sprite { get { return sprite; } set { sprite = value; } }
-
         private Texture2D missileTexture; // Should be Sprite?
         public Texture2D MissileTexture { get { return missileTexture; } set { missileTexture = value; } }
 
         private List<Missile> missiles = new List<Missile>();
         public List<Missile> Missiles { get { return missiles; } }
 
-        private bool hasController;
-        public bool HasController { get { return hasController; } set { hasController = value; } }
-
-        private bool hasKeyboard;
-        public bool HasKeyboard { get { return hasKeyboard; } set { hasKeyboard = value; } }
-
-        private int controllerIndex;
-        public int ControllerIndex { get { return controllerIndex; } set { controllerIndex = value; } }
-
         private List<Missile> missilesToRemove = new List<Missile>();
         private Missile missileToRemove;
 
-        private InputHelper inputHelper = new InputHelper();
+        //private Sprite sprite; //Replaces Texture2D
+        //public Sprite Sprite { get { return sprite; } set { sprite = value; } }
+
 
         //Default constructor
         public Ship()
@@ -49,50 +40,24 @@ namespace Game1
 
         }
 
-        public Ship(float xPos, float yPos, bool hasKeyboard, bool hasController)
-        {
-            XPos = xPos;
-            YPos = yPos;
-            HasKeyboard = hasKeyboard;
-            HasController = hasController;
-        }
-
-        public Ship(float xPos, float yPos, bool hasKeyboard, bool hasController, int controllerIndex)
-        {
-            XPos = xPos;
-            YPos = yPos;
-            HasKeyboard = hasKeyboard;
-            HasController = hasController;
-            ControllerIndex = controllerIndex;
-        }
-
-        /*
-        public Ship(Sprite sprite)
-        {
-            Sprite = sprite //Rätt?
-            X = 10;
-            Y = 10;
-        }
-        */
-
         public void Move(float x, float y)
         {
             XPos += x;
             YPos += y;
         }
 
-        public void ResetPosition()
-        {
-            XPos = 10;
-            YPos = 10;
-        }
+        private int missileCooldownMS = 500;
+        private GameTime oldTime;
 
-        public void FireMain()
+        public void FireMain(GameTime newTime)
         {
-            if (this.Missiles.Count < 2)
+            if ((newTime.ElapsedGameTime.Milliseconds - oldTime.ElapsedGameTime.Milliseconds) > missileCooldownMS)
             {
-                missiles.Add(new Missile(XPos - 5, YPos, missileTexture));
-                missiles.Add(new Missile(XPos + 40, YPos, missileTexture));
+                if (this.Missiles.Count < 4)
+                {
+                    Missiles.Add(new Missile(XPos - 5, YPos, MissileTexture));
+                    Missiles.Add(new Missile(XPos + 40, YPos, MissileTexture));
+                }
             }
         }
 
@@ -131,16 +96,6 @@ namespace Game1
                     missilesToRemove.Add(m);
                 }
             }
-        }
-
-        public void FireSecondary()
-        {
-            System.Console.WriteLine("Whoooosh!");
-        }
-
-        public void Explode()
-        {
-            System.Console.WriteLine("i died :(");
         }
     }
 }
